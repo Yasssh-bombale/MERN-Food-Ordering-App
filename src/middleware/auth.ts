d2051bfbec59abb6631f88148f32bb1b41c7f833
Yash Vijay Bombale <yashbombale@gmail.com>
2024-03-10 19:05:58 +0530

    Create middleware jwtParse for storing req.userId and req.auth0Id

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,7 +1,45 @@
+import { Request, Response, NextFunction } from "express";
 import { auth } from "express-oauth2-jwt-bearer";
+import jwt from "jsonwebtoken";
+import User from "../models/user.model";
+
+declare global {
+  namespace Express {
+    interface Request {
+      userId: string;
+      auth0Id: string;
+    }
+  }
+}
 
 export const jwtCheck = auth({
   audience: "mern-food-ordering-app-api",
   issuerBaseURL: "https://dev-caduw53ujxaqbg0n.us.auth0.com/",
   tokenSigningAlg: "RS256",
 });
+
+export const jwtParse = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { authorization } = req.headers;
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    return res.sendStatus(401);
+  }
+  const token = authorization.split(" ")[1];
+
+  try {
+    const decode = jwt.decode(token) as jwt.JwtPayload;
+    const auth0Id = decode.sub;
+    const user = await User.findOne({ auth0Id });
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+    req.auth0Id = auth0Id as string;
+    req.userId = user._id.toString();
+    next();
+  } catch (error) {
+    return res.sendStatus(401);
+  }
+};
