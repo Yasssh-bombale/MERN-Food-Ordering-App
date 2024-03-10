e1f8a420e7305619f338329c47f01f8b313f5fab
Yash Vijay Bombale <yashbombale@gmail.com>
2024-03-10 12:43:38 +0530

    validate auth0 token

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import { createCurrentUser } from "../controllers/user.controller";
+import { jwtCheck } from "../middleware/auth";
 
 const router = express.Router();
 
-router.post("/create", createCurrentUser);
+router.post("/create", jwtCheck, createCurrentUser);
 
 export default router;
