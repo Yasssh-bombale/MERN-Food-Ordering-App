118c6add25236f9530935563684f1a356ed89be1
Yash Vijay Bombale <yashbombale@gmail.com>
2024-03-12 08:38:53 +0530

    fixed bugs in validation.ts

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 
-const handleValidationError = async (
+const handleValidationErrors = async (
   req: Request,
   res: Response,
   next: NextFunction
@@ -14,12 +14,12 @@ const handleValidationError = async (
 };
 
 export const validateMyUserRequest = [
+  body("name").isString().notEmpty().withMessage("Name must be a string"),
   body("addressLine1")
     .isString()
     .notEmpty()
-    .withMessage("Addressline1 must be a string"),
-  body("name").isString().isEmpty().withMessage("Name must be a string"),
-  body("city").isString().isEmpty().withMessage("City must be a string"),
-  body("country").isString().isEmpty().withMessage("Country must be a string"),
-  handleValidationError,
+    .withMessage("AddressLine1 must be a string"),
+  body("city").isString().notEmpty().withMessage("City must be a string"),
+  body("country").isString().notEmpty().withMessage("Country must be a string"),
+  handleValidationErrors,
 ];
